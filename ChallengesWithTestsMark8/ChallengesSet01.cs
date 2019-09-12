42e112b79fbcf744e2deea01d59a29bd881812f4
nichols pickett <npickett02@gmail.com>
2019-09-12 09:39:52 -0500

    Completed

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,37 +6,58 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();
+            if (num1 == num2)
+                {
+                return true;
+                }
+            else
+                {
+                return false;
+                }
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            var x = (minuend - subtrahend);
+            return x;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            var z = (number1 + number2);
+            return z;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 > number2)
+            {
+                return number2;
+            }
+            else
+            {
+                return number1;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            var y = (factor1 * factor2);
+            return y;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+            return $"Hello, {nameOfPerson}!";
         }
 
         public string GetHey()
-        {
-            throw new NotImplementedException();
+        { 
+            return "HEY!";
         }
     }
 }
